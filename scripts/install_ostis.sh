f44e3497513aff3be3fd111e619003eaf3f39d06
ValikVityaz <valik.vityaz@gmail.com>
2020-12-03 17:11:15 +0300

    build(common): apply relevant changes from ostis-example-app

--- scripts/install_ostis.sh
+++ scripts/install_ostis.sh
@@ -1,63 +1,66 @@
 #!/bin/bash
 
+APP_ROOT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd)
+PLATFORM_PATH="${APP_ROOT_PATH}/ostis-web-platform"
+WORKING_PATH=$(pwd)
+PYTHON_PATH="${APP_ROOT_PATH}"/problem-solver/py/services
+PLATFORM_REPO="https://github.com/ostis-dev/ostis-web-platform.git"
+
 prepare_platform()
 {
-	cd scripts
+	cd "${PLATFORM_PATH}"/scripts
 	./prepare.sh
-	cd ..
+	cd "${PLATFORM_PATH}"
 }
 
 prepare_platform_without_build()
 {
-	cd scripts
+	cd "${PLATFORM_PATH}"/scripts
 	./prepare.sh no_build_kb no_build_sc_machine
-	cd ..
+	cd "${PLATFORM_PATH}"
 }
 
 include_kb()
 {
+	cd "${PLATFORM_PATH}"
 	rm ./ims.ostis.kb/ui/ui_start_sc_element.scs
 	rm -rf ./kb/menu
 	echo "../kb" >> ./repo.path
 	echo "../problem-solver" >> ./repo.path
 	cd scripts
 	./build_kb.sh
-	cd ..
+	cd "${PLATFORM_PATH}"
 }
 
-include_kpm()
+include_problem_solver()
 {
-	cd sc-machine
-	sed -i '\|build/problem-solver/cxx|d' ./CMakeLists.txt
-	sed -i '\|build/problem-solver/py|d' ./CMakeLists.txt
-	cat ../../scripts/sc_machine_cmake_file_ext.txt >> ./CMakeLists.txt
+	if ! grep -q "${PYTHON_PATH}" "${PLATFORM_PATH}/sc-machine/config/config.ini.in";
+	then
+		PYTHON_PATH_ESCAPED="$(echo "${PYTHON_PATH}" | sed -e 's/[/]/\\&/g')"
+		sed -i "/modules_path/ s/$/;${PYTHON_PATH_ESCAPED}/" "${PLATFORM_PATH}/sc-machine/config/config.ini.in"
+	fi
 
-	cd config
-	sed -i '/python/d' ./config.ini.in
-	sed -i '/modules_path/d' ./config.ini.in
-	echo -e '[python]' >> ./config.ini.in
-	echo 'modules_path = ${SC_MACHINE_ROOT}/sc-kpm/sc-python/services;${SC_MACHINE_ROOT}/../../problem-solver/py/services' >> ./config.ini.in
-	cd ..
-
-	cd ./scripts
-	./make_all.sh
-	cat ../bin/config.ini >> ../../config/sc-web.ini
-	cd ../..
+	cd "${APP_ROOT_PATH}"/scripts
+	./build_problem_solver.sh
+	cat "${PLATFORM_PATH}"/sc-machine/bin/config.ini >> "${PLATFORM_PATH}"/config/sc-web.ini
+	cd "${PLATFORM_PATH}"
 }
 
-cd ..
-if [ -d "ostis" ];
+cd "${APP_ROOT_PATH}"
+if [ -d "${PLATFORM_PATH}" ];
 	then
 		echo -en "Update OSTIS platform\n"
-		cd ostis
+		cd "${PLATFORM_PATH}"
 		git pull
 		prepare_platform
 	else
 		echo -en "Install OSTIS platform\n"
-		git clone https://github.com/ShunkevichDV/ostis.git
-		cd ostis
+		git clone ${PLATFORM_REPO}
+		cd "${PLATFORM_PATH}"
 		git checkout 0.6.0
 		prepare_platform_without_build
-		include_kpm
+		include_problem_solver
 		include_kb
 fi
+
+cd "${WORKING_PATH}"
