22008641e63e58662f51e2ebc09153bbdccdd02f
RuslanKorshunov <korshunov19101998@gmail.com>
2021-03-17 20:05:02 +0300

    feature(problem-solver): create TemplateManager

--- problem-solver/cxx/inferenceModule/manager/DirectInferenceManager.cpp
+++ problem-solver/cxx/inferenceModule/manager/DirectInferenceManager.cpp
@@ -9,25 +9,23 @@
 #include <sc-memory/sc_addr.hpp>
 #include <sc-agents-common/utils/GenerationUtils.hpp>
 #include <sc-agents-common/utils/IteratorUtils.hpp>
-#include <sc-agents-common/keynodes/coreKeynodes.hpp>
 #include <sc-agents-common/utils/LogicRuleUtils.hpp>
 
 #include "utils/ContainersUtils.hpp"
 
 using namespace inference;
 using namespace utils;
-using namespace scAgentsCommon;
 
 DirectInferenceManager::DirectInferenceManager(ScMemoryContext * ms_context) : ms_context(ms_context)
 {
   this->solutionTreeManager = new SolutionTreeGenerator(ms_context);
+  this->templateManager = new TemplateManager(ms_context);
 }
 
 DirectInferenceManager::~DirectInferenceManager()
 {
   delete this->solutionTreeManager;
 }
-
 
 ScAddr DirectInferenceManager::applyInference(
       const ScAddr & targetStatement,
@@ -83,8 +81,8 @@ bool DirectInferenceManager::useRule(ScAddr const & rule, vector<ScAddr> const &
   SC_LOG_DEBUG("Trying to use rule: " + ms_context->HelperGetSystemIdtf(rule));
   bool isUsed = false;
   ScAddr ifStatement = LogicRuleUtils::getIfStatement(ms_context, rule);
-  std::vector<ScTemplateParams> ifStatementParamsList = createTemplateParamsList(ifStatement, argumentList);
-  SC_LOG_DEBUG("Created " + std::to_string(ifStatementParamsList.size()) + " statement params variants");
+  vector<ScTemplateParams> ifStatementParamsList = templateManager->createTemplateParamsList(ifStatement, argumentList);
+  SC_LOG_DEBUG("Created " + to_string(ifStatementParamsList.size()) + " statement params variants");
   for (const auto& ifStatementParams : ifStatementParamsList)
   {
     ScTemplate ifStatementTemplate;
@@ -128,160 +126,6 @@ bool DirectInferenceManager::useRule(ScAddr const & rule, vector<ScAddr> const &
   return isUsed;
 }
 
-std::vector<ScTemplateParams>
-DirectInferenceManager::createTemplateParamsList(ScAddr const & scTemplate, const vector<ScAddr> & argumentList)
-{
-  std::vector<std::map<ScAddr, string, AddrComparator>> replacementsList;
-  ScIterator3Ptr varIterator = ms_context->Iterator3(
-        scTemplate,
-        ScType::EdgeAccessConstPosPerm,
-        ScType::NodeVar);
-  while (varIterator->Next())//падает тут
-  {
-    ScAddr var = varIterator->Get(2);
-    string varName = ms_context->HelperGetSystemIdtf(var);
-    std::vector<ScAddr> argumentOfVarList;
-    ScIterator5Ptr classesIterator = ms_context->Iterator5(
-          ScType::NodeConstClass,
-          ScType::EdgeAccessVarPosPerm,
-          var,
-          ScType::EdgeAccessConstPosPerm,
-          scTemplate);
-    while (classesIterator->Next())
-    {
-      ScAddr varClass = classesIterator->Get(0);
-      for (auto & argument : argumentList)
-      {
-        if ((ms_context->HelperCheckEdge(varClass, argument, ScType::EdgeAccessConstPosPerm)) && (argument.IsValid()))
-        {
-          argumentOfVarList.push_back(argument);
-        }
-      }
-    }
-    if(!argumentList.empty())
-    {
-      addVarToReplacementsList(replacementsList, varName, argumentOfVarList);
-    }
-  }
-
-  return createTemplateParamsList(replacementsList);
-}
-
-void DirectInferenceManager::addVarToReplacementsList(
-      std::vector<std::map<ScAddr, string, AddrComparator>> & replacementsList,
-      string & varName,
-      std::vector<ScAddr> & argumentOfVarList)
-{
-  if(!argumentOfVarList.empty())
-  {
-    std::vector<std::map<ScAddr, string, AddrComparator>> newReplacementsList;
-
-    if (!replacementsList.empty())
-    {
-      for (auto & replacementsMap : replacementsList)
-      {
-        for (auto & argumentOfVar : argumentOfVarList)
-        {
-          if(!replacementsMap.count(argumentOfVar))
-          {
-            std::map<ScAddr, string, AddrComparator> newReplacementsMap = replacementsMap;
-            newReplacementsMap.insert(std::make_pair(argumentOfVar, varName));
-            newReplacementsList.push_back(newReplacementsMap);
-          }
-        }
-      }
-    }
-    else
-    {
-      for (auto & argumentOfVar : argumentOfVarList)
-      {
-        std::map<ScAddr, string, AddrComparator> newReplacementsMap;
-        newReplacementsMap.insert(std::make_pair(argumentOfVar, varName));
-        newReplacementsList.push_back(newReplacementsMap);
-      }
-    }
-
-    replacementsList = newReplacementsList;
-  }
-}
-
-std::vector<ScTemplateParams>
-DirectInferenceManager::createTemplateParamsList(std::vector<std::map<ScAddr, string, AddrComparator>> & replacementsList)
-{
-  SC_LOG_DEBUG("Creating template params fo rule usage")
-  std::vector<ScTemplateParams> templateParamsList;
-  for (auto & replacementsMap : replacementsList)
-  {
-    ScTemplateParams scTemplateParams;
-    for (std::pair<ScAddr, string> replacement : replacementsMap)
-    {
-      SC_LOG_DEBUG(replacement.second + " is " + std::to_string(replacement.first.GetRealAddr().seg) + "/" + std::to_string(replacement.first.GetRealAddr().offset));
-      scTemplateParams.Add(replacement.second, replacement.first);
-    }
-    templateParamsList.push_back(scTemplateParams);
-    SC_LOG_DEBUG("***")
-  }
-  SC_LOG_DEBUG("Created template params fo rule usage")
-  return templateParamsList;
-}
-
-ScTemplateParams
-DirectInferenceManager::createTemplateParams(ScAddr const & scTemplate, const vector<ScAddr> & argumentList)
-{
-  std::map<ScAddr, string, AddrComparator> replacementsMap;
-  SC_LOG_DEBUG("***\ncreating template params\ntemplate name: " + ms_context->HelperGetSystemIdtf(scTemplate));
-
-  ScIterator3Ptr varIterator = ms_context->Iterator3(
-        scTemplate,
-        ScType::EdgeAccessConstPosPerm,
-        ScType::NodeVar);
-  while (varIterator->Next())
-  {
-    ScAddr var = varIterator->Get(2);
-    SC_LOG_DEBUG("found variable: " + ms_context->HelperGetSystemIdtf(var));
-    ScAddr argumentOfVar;
-    ScIterator5Ptr classesIterator = ms_context->Iterator5(
-          ScType::NodeConstClass,
-          ScType::EdgeAccessVarPosPerm,
-          var,
-          ScType::EdgeAccessConstPosPerm,
-          scTemplate);
-    while (classesIterator->Next())
-    {
-      ScAddr varClass = classesIterator->Get(0);
-      for (auto & argument : argumentList)
-      {
-        if (ms_context->HelperCheckEdge(varClass, argument, ScType::EdgeAccessConstPosPerm))
-        {
-          if(!replacementsMap.count(argument))
-          {
-            argumentOfVar = argument;
-            break;
-          }
-        }
-      }
-      if (argumentOfVar.IsValid())
-      {
-        string varName = ms_context->HelperGetSystemIdtf(var);
-        replacementsMap.insert(std::make_pair(argumentOfVar, varName));
-        SC_LOG_DEBUG("variable class: " + ms_context->HelperGetSystemIdtf(varClass));
-        SC_LOG_DEBUG(varName + " is " + std::to_string(argumentOfVar.GetRealAddr().seg) + "/" + std::to_string(argumentOfVar.GetRealAddr().offset));
-        break;
-      }
-    }
-  }
-  SC_LOG_DEBUG("***");
-
-  ScTemplateParams templateParams;
-  for (const auto& replacement : replacementsMap)
-  {
-    templateParams.Add(replacement.second, replacement.first);
-  }
-  return templateParams;
-}
-
-
-
 bool DirectInferenceManager::generateStatement(ScAddr const & statement, ScTemplateParams const & templateParams)
 {
   bool result = false;
@@ -301,7 +145,7 @@ bool DirectInferenceManager::generateStatement(ScAddr const & statement, ScTempl
 bool DirectInferenceManager::isTargetAchieved(ScAddr const & targetStatement, vector<ScAddr> const & argumentList)
 {
   bool result = false;
-  ScTemplateParams templateParams = createTemplateParams(targetStatement, argumentList);
+  ScTemplateParams templateParams = templateManager->createTemplateParams(targetStatement, argumentList);
   ScTemplate targetStatementTemplate;
   if (ms_context->HelperBuildTemplate(targetStatementTemplate, targetStatement, templateParams))
   {
---
+++ problem-solver/cxx/inferenceModule/manager/TemplateManager.hpp
@@ -0,0 +1,44 @@
+/*
+* This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+* Distributed under the MIT License
+* (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+*/
+
+#pragma once
+
+#include <vector>
+
+#include "comparator/AddrComparator.hpp"
+#include "sc-memory/sc_memory.hpp"
+
+using namespace std;
+
+namespace inference
+{
+class TemplateManager
+{
+public:
+  explicit TemplateManager(ScMemoryContext * ms_context);
+
+  std::vector<ScTemplateParams> createTemplateParamsList(
+        ScAddr const & scTemplate,
+        const vector<ScAddr> & argumentList);
+
+  ScTemplateParams createTemplateParams(
+        ScAddr const & scTemplate,
+        const vector<ScAddr> & argumentList);
+
+  ~ TemplateManager() = default;
+
+private:
+  ScMemoryContext * context;
+
+  static void addVarToReplacementsList(
+        std::vector<std::map<ScAddr, string, AddrComparator>> & replacementsList,
+        string & varName,
+        std::vector<ScAddr> & argumentOfVarList);
+
+  static vector<ScTemplateParams> createTemplateParamsList(
+        vector<map<ScAddr, string, AddrComparator>> & replacementsList);
+};
+}
