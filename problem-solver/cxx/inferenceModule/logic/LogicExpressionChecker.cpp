a5f411ad79f1d608461a7ddd0f3598771410791c
wcobalt <cobalt.itech@gmail.com>
2021-12-09 02:46:00 +0300

    The update of complex logical expression support

--- problem-solver/cxx/inferenceModule/logic/LogicExpressionChecker.cpp
+++ problem-solver/cxx/inferenceModule/logic/LogicExpressionChecker.cpp
@@ -1,4 +1,207 @@
-//
-// Created by wcobalt on 12/4/21.
-//
+/*
+* This source file is part of an OSTIS project. For the latest info, see http://ostis.net
+* Distributed under the MIT License
+* (See accompanying file COPYING.MIT or copy at http://opensource.org/licenses/MIT)
+*/
 
+#include "LogicExpressionChecker.h"
+
+AndExpressionNode::AndExpressionNode(std::unique_ptr<LogicExpressionNode> opA,
+                  std::unique_ptr<LogicExpressionNode> opB)
+                  : leftOp(std::move(opA)), rightOp(std::move(opB)) {}
+
+LogicExpressionResult AndExpressionNode::check(ScTemplateParams params) const {
+    bool finalResult = false;
+
+    auto result = leftOp->check(params);
+    LogicExpressionResult result2;
+    if (result.result) {
+        result2 = rightOp->check(params);
+
+        if (result2.result)
+            finalResult = true;
+    }
+
+    bool tsr1 = result.hasTemplateSearchResult;
+    bool tsr2 = result2.hasTemplateSearchResult;
+
+    LogicExpressionResult res;
+    res.result = finalResult;
+    res.hasTemplateSearchResult = finalResult ? tsr1 || tsr2 : false;
+    res.templateSearchResult = tsr1 ? result.templateSearchResult :
+                               (tsr2 ? result2.templateSearchResult :
+                                ScTemplateSearchResultItem(nullptr, nullptr));
+    res.templateItself = result.templateItself;
+
+    return res;
+}
+
+OrExpressionNode::OrExpressionNode(std::unique_ptr<LogicExpressionNode> opA,
+                                   std::unique_ptr<LogicExpressionNode> opB)
+                                   : leftOp(std::move(opA)), rightOp(std::move(opB)) {}
+
+LogicExpressionResult OrExpressionNode::check(ScTemplateParams params) const {
+    bool finalResult = true;
+
+    auto result = leftOp->check(params);
+    LogicExpressionResult result2;
+    if (!result.result) {
+        result2 = rightOp->check(params);
+
+        if (!result2.result)
+            finalResult = false;
+    }
+
+    bool tsr1 = result.hasTemplateSearchResult;
+    bool tsr2 = result2.hasTemplateSearchResult;
+
+    LogicExpressionResult res;
+    res.result = finalResult;
+    res.hasTemplateSearchResult = finalResult ? tsr1 || tsr2 : false;
+    res.templateSearchResult = tsr1 ? result.templateSearchResult :
+                               (tsr2 ? result2.templateSearchResult :
+                                ScTemplateSearchResultItem(nullptr, nullptr));
+    res.templateItself = result.templateItself;
+
+    return res;
+}
+
+NotExpressionNode::NotExpressionNode(std::unique_ptr<LogicExpressionNode> op)
+                                     : op(std::move(op)) {}
+
+LogicExpressionResult NotExpressionNode::check(ScTemplateParams params) const {
+    auto result = op->check(params);
+
+    result.result = !result.result;
+
+    return result;
+}
+
+TemplateExpressionNode::TemplateExpressionNode(ScAddr templateToCheck,
+                                               inference::TemplateSearcher* templateSearcher,
+                                               ScMemoryContext* context)
+                                               : templateToCheck(templateToCheck),
+                                               templateSearcher(templateSearcher),
+                                               context(context) {}
+
+LogicExpressionResult TemplateExpressionNode::check(ScTemplateParams params) const {
+    std::vector<ScTemplateSearchResultItem> searchResult =
+            templateSearcher->searchTemplate(templateToCheck, params);
+
+    SC_LOG_DEBUG(context->HelperGetSystemIdtf(templateToCheck) + ": "
+                + std::to_string(!searchResult.empty()))
+
+    if (!searchResult.empty()) {
+        return {true,
+                true,
+                searchResult[0],
+                templateToCheck};
+    } else {
+        return {false,
+                false,
+                {nullptr, nullptr},
+                templateToCheck};
+    }
+}
+
+LogicExpression::LogicExpression(ScAddr condition, ScMemoryContext* context,
+                                 inference::TemplateSearcher* templateSearcher,
+                                 inference::TemplateManager * templateManager,
+                                 std::vector<ScAddr> argumentList)
+                                 : conditionRoot(condition), context(context),
+                                 templateSearcher(templateSearcher), templateManager(templateManager),
+                                 argumentList(std::move(argumentList)) {
+    atomicLogicalFormula = context->HelperResolveSystemIdtf("atomic_logical_formula");
+    nrelDisjunction = context->HelperResolveSystemIdtf("nrel_disjunction");
+    nrelConjunction = context->HelperResolveSystemIdtf("nrel_conjunction");
+    nrelNegation = context->HelperResolveSystemIdtf("nrel_negation");
+}
+
+void LogicExpression::build() {
+    //collect all the templates within if condition
+    //generate for them sets of ScTemplateParams
+    //choose the set when IsEmpty() is false, memorize the template the set was generated for
+
+    root = build(conditionRoot);
+}
+
+std::unique_ptr<LogicExpressionNode> LogicExpression::build(ScAddr node) {
+    ScIterator3Ptr atomicFormula = context->Iterator3(
+            atomicLogicalFormula,
+            ScType::EdgeAccessConstPosPerm,
+            node);
+
+    ScIterator3Ptr conjunction = context->Iterator3(
+            nrelConjunction,
+            ScType::EdgeAccessConstPosPerm,
+            node);
+
+    ScIterator3Ptr disjunction = context->Iterator3(
+            nrelDisjunction,
+            ScType::EdgeAccessConstPosPerm,
+            node);
+
+    ScIterator3Ptr negation = context->Iterator3(
+            nrelNegation,
+            ScType::EdgeAccessConstPosPerm,
+            node);
+
+    ScIterator3Ptr operands = context->Iterator3(
+            node,
+            ScType::EdgeAccessConstPosPerm,
+            ScType::Unknown);
+
+    if (atomicFormula->Next()) {
+        //atomic formula, make TemplateExpressionNode
+        SC_LOG_DEBUG(context->HelperGetSystemIdtf(node) + " is a template")
+        std::vector<ScTemplateParams> params
+                = templateManager->createTemplateParamsList(node, argumentList);
+
+        if (!params.empty() && paramsSet.empty()) {
+            paramsSet = std::move(params);
+        }
+
+        return std::make_unique<TemplateExpressionNode>(node, templateSearcher, context);
+    } else if (conjunction->Next()) {
+        //and, make AndExpressionNode
+        SC_LOG_DEBUG(context->HelperGetSystemIdtf(node) + " is a conjunction")
+        if (operands->Next()) {
+            std::unique_ptr<LogicExpressionNode> opA =
+                    build(operands->Get(2));
+
+            if (operands->Next()) {
+                std::unique_ptr<LogicExpressionNode> opB =
+                        build(operands->Get(2));
+
+                return std::make_unique<AndExpressionNode>(std::move(opA),
+                                                           std::move(opB));
+            }
+        }
+    } else if (disjunction->Next()) {
+        //or, make OrExpressionNode
+        SC_LOG_DEBUG(context->HelperGetSystemIdtf(node) + " is a disjunction")
+        if (operands->Next()) {
+            std::unique_ptr<LogicExpressionNode> opA =
+                    build(operands->Get(2));
+
+            if (operands->Next()) {
+                std::unique_ptr<LogicExpressionNode> opB =
+                        build(operands->Get(2));
+
+                return std::make_unique<OrExpressionNode>(std::move(opA),
+                                                          std::move(opB));
+            }
+        }
+    } else if (negation->Next()) {
+        //not, make NotExpressionNode
+        SC_LOG_DEBUG(context->HelperGetSystemIdtf(node) + " is a not")
+        if (operands->Next()) {
+            std::unique_ptr<LogicExpressionNode> op =
+                    build(operands->Get(2));
+
+            return std::make_unique<NotExpressionNode>(std::move(op));
+        }
+    }
+
+    throw runtime_error("[INFERENCE_MODULE] Unrecognized element of condition tree");
+}
--- problem-solver/cxx/inferenceModule/logic/LogicExpressionChecker.h
+++ problem-solver/cxx/inferenceModule/logic/LogicExpressionChecker.h
@@ -9,12 +9,19 @@
 #include <utility>
 #include <vector>
 #include <sc-memory/sc_template.hpp>
+#include <manager/TemplateManager.hpp>
 #include "searcher/TemplateSearcher.hpp"
+
+struct LogicExpressionResult {
+    bool result;
+    bool hasTemplateSearchResult;
+    ScTemplateSearchResultItem templateSearchResult{nullptr, nullptr};
+    ScAddr templateItself;
+};
 
 class LogicExpressionNode {
 public:
-    virtual std::tuple<bool, bool, ScTemplateSearchResultItem, ScAddr>
-            check(ScAddr referenceTemplate, ScTemplateParams params) const = 0;
+    virtual LogicExpressionResult check(ScTemplateParams params) const = 0;
 
     virtual std::string toString() const = 0;
 };
@@ -22,44 +29,12 @@ public:
 class AndExpressionNode : public LogicExpressionNode {
 public:
     AndExpressionNode(std::unique_ptr<LogicExpressionNode> opA,
-                      std::unique_ptr<LogicExpressionNode> opB) :
-            leftOp(std::move(opA)),
-            rightOp(std::move(opB)) {}
+                      std::unique_ptr<LogicExpressionNode> opB);
 
-    std::tuple<bool, bool, ScTemplateSearchResultItem, ScAddr>
-        check(ScAddr referenceTemplate, ScTemplateParams params) const override {
-
-        bool finalResult = false;
-
-        auto result = leftOp->check(referenceTemplate, params);
-        std::tuple<bool, bool, ScTemplateSearchResultItem, ScAddr> result2
-            {false, false, {nullptr, nullptr}, ScAddr()};
-        if (std::get<0>(result)) {
-            result2 = rightOp->check(referenceTemplate, params);
-
-            if (std::get<0>(result2))
-                finalResult = true;
-        }
-
-        bool firstResultHasReferenceTemplate = std::get<1>(result);
-        bool secondResultHasReferenceTemplate = std::get<1>(result2);
-
-        return {finalResult,
-
-            finalResult
-            ?  firstResultHasReferenceTemplate || secondResultHasReferenceTemplate
-            : false,
-
-                firstResultHasReferenceTemplate ? std::get<2>(result) :
-                (secondResultHasReferenceTemplate ? std::get<2>(result2) :
-                ScTemplateSearchResultItem(nullptr, nullptr)),
-
-                std::get<3>(result)
-        };
-    }
+    LogicExpressionResult check(ScTemplateParams params) const override;
 
     std::string toString() const {
-        return "AND( " + leftOp->toString() + ", " + rightOp->toString() + ")";
+        return "AND(" + leftOp->toString() + ", " + rightOp->toString() + ")";
     }
 
 private:
@@ -70,44 +45,12 @@ private:
 class OrExpressionNode : public LogicExpressionNode {
 public:
     OrExpressionNode(std::unique_ptr<LogicExpressionNode> opA,
-                     std::unique_ptr<LogicExpressionNode> opB) :
-            leftOp(std::move(opA)),
-            rightOp(std::move(opB)) {}
+                     std::unique_ptr<LogicExpressionNode> opB);
 
-    std::tuple<bool, bool, ScTemplateSearchResultItem, ScAddr>
-        check(ScAddr referenceTemplate, ScTemplateParams params) const override {
-
-        bool finalResult = true;
-
-        auto result = leftOp->check(referenceTemplate, params);
-        std::tuple<bool, bool, ScTemplateSearchResultItem, ScAddr> result2
-                {false, false, {nullptr, nullptr}, ScAddr()};
-        if (!std::get<0>(result)) {
-            result2 = rightOp->check(referenceTemplate, params);
-
-            if (!std::get<0>(result2))
-                finalResult = false;
-        }
-
-        bool firstResultHasReferenceTemplate = std::get<1>(result);
-        bool secondResultHasReferenceTemplate = std::get<1>(result2);
-
-        return {finalResult,
-
-                finalResult
-                ?  firstResultHasReferenceTemplate || secondResultHasReferenceTemplate
-                : false,
-
-                firstResultHasReferenceTemplate ? std::get<2>(result) :
-                (secondResultHasReferenceTemplate ? std::get<2>(result2) :
-                 ScTemplateSearchResultItem(nullptr, nullptr)),
-
-                std::get<3>(result)
-        };
-    }
+    LogicExpressionResult check(ScTemplateParams params) const override;
 
     std::string toString() const {
-        return "OR( " + leftOp->toString() + ", " + rightOp->toString() + ")";
+        return "OR(" + leftOp->toString() + ", " + rightOp->toString() + ")";
     }
 
 private:
@@ -117,20 +60,12 @@ private:
 
 class NotExpressionNode : public LogicExpressionNode {
 public:
-    explicit NotExpressionNode(std::unique_ptr<LogicExpressionNode> op) : op(std::move(op)) {}
+    explicit NotExpressionNode(std::unique_ptr<LogicExpressionNode> op);
 
-    std::tuple<bool, bool, ScTemplateSearchResultItem, ScAddr>
-        check(ScAddr referenceTemplate, ScTemplateParams params) const override {
-
-        auto result = op->check(referenceTemplate, params);
-
-        std::get<0>(result) = !std::get<0>(result);
-
-        return result;
-    }
+    LogicExpressionResult check(ScTemplateParams params) const override;
 
     std::string toString() const {
-        return "NOT( " + op->toString() + ")";
+        return "NOT(" + op->toString() + ")";
     }
 
 private:
@@ -140,24 +75,10 @@ private:
 class TemplateExpressionNode : public LogicExpressionNode {
 public:
     TemplateExpressionNode(ScAddr templateToCheck,
-            inference::TemplateSearcher* templateSearcher, ScMemoryContext* context)
-            : templateToCheck(templateToCheck), templateSearcher(templateSearcher), context(context) {}
+                           inference::TemplateSearcher* templateSearcher,
+                           ScMemoryContext* context);
 
-    std::tuple<bool, bool, ScTemplateSearchResultItem, ScAddr>
-    check(ScAddr referenceTemplate, ScTemplateParams params) const override {
-        vector<ScTemplateSearchResultItem> searchResult =
-                templateSearcher->searchTemplate(templateToCheck, params);
-
-        SC_LOG_INFO(context->HelperGetSystemIdtf(templateToCheck) + ": "
-            + std::to_string(!searchResult.empty()))
-
-        if (!searchResult.empty()) {
-            return {true, true, searchResult[0], templateToCheck};
-        } else {
-            return {false, false,
-                    {nullptr, nullptr}, templateToCheck};
-        }
-    }
+    LogicExpressionResult check(ScTemplateParams params) const override;
 
     std::string toString() const {
         std::string sysIdtf = context->HelperGetSystemIdtf(templateToCheck);
@@ -175,125 +96,20 @@ private:
 class LogicExpression {
 public:
     LogicExpression(ScAddr condition, ScMemoryContext* context,
-            inference::TemplateSearcher* templateSearcher, inference::TemplateManager * templateManager,
-                    vector<ScAddr> argumentList)
-        : conditionRoot(condition), context(context),
-        templateSearcher(templateSearcher), templateManager(templateManager),
-        argumentList(std::move(argumentList)){
+                    inference::TemplateSearcher* templateSearcher,
+                    inference::TemplateManager * templateManager,
+                    std::vector<ScAddr> argumentList);
 
-        atomicLogicalFormula = context->HelperResolveSystemIdtf("atomic_logical_formula");
-        nrelDisjunction = context->HelperResolveSystemIdtf("nrel_disjunction");
-        nrelConjunction = context->HelperResolveSystemIdtf("nrel_conjunction");
-        nrelNegation = context->HelperResolveSystemIdtf("nrel_negation");
-    }
-
-    void build() {
-        //collect all the templates within if condition
-        //generate for them sets of ScTemplateParams
-        //choose the set when IsEmpty() is false, memorize the template the set was generated for
-
-        root = build(conditionRoot);
-    }
+    void build();
 
     std::string toString() {
         return root->toString();
     }
 
-    std::unique_ptr<LogicExpressionNode> build(ScAddr node) {
-        SC_LOG_INFO(context->HelperGetSystemIdtf(node))
-        ScIterator3Ptr atomicFormula = context->Iterator3(
-                atomicLogicalFormula,
-                ScType::EdgeAccessConstPosPerm,
-                node);
-
-        ScIterator3Ptr conjunction = context->Iterator3(
-                nrelConjunction,
-                ScType::EdgeAccessConstPosPerm,
-                node);
-
-        ScIterator3Ptr disjunction = context->Iterator3(
-                nrelDisjunction,
-                ScType::EdgeAccessConstPosPerm,
-                node);
-
-        ScIterator3Ptr negation = context->Iterator3(
-                nrelNegation,
-                ScType::EdgeAccessConstPosPerm,
-                node);
-
-        ScIterator3Ptr operands = context->Iterator3(
-                node,
-                ScType::EdgeAccessConstPosPerm,
-                ScType::Unknown);
-
-        if (atomicFormula->Next()) {
-            //atomic formula, make TemplateExpressionNode
-            SC_LOG_INFO("template")
-            std::vector<ScTemplateParams> params
-                = templateManager->createTemplateParamsList(node, argumentList);
-            SC_LOG_INFO("C: " + std::to_string(params.size()));
-            if (!params.empty() && paramsSet.empty()) {
-                //SC_LOG_INFO("ref: " + context->HelperGetSystemIdtf(referenceTemplate));
-                paramsSet = std::move(params);
-            }
-
-            return std::make_unique<TemplateExpressionNode>(node, templateSearcher, context);
-        } else if (conjunction->Next()) {
-            SC_LOG_INFO("cojunction")
-
-            //and, make AndExpressionNode
-            if (operands->Next()) {
-                std::unique_ptr<LogicExpressionNode> opA =
-                        build(operands->Get(2));
-
-                if (operands->Next()) {
-                    std::unique_ptr<LogicExpressionNode> opB =
-                            build(operands->Get(2));
-
-                    return std::make_unique<AndExpressionNode>(std::move(opA),
-                            std::move(opB));
-                }
-            }
-        } else if (disjunction->Next()) {
-            //or, make OrExpressionNode
-            SC_LOG_INFO("disj")
-
-            if (operands->Next()) {
-                std::unique_ptr<LogicExpressionNode> opA =
-                        build(operands->Get(2));
-
-                if (operands->Next()) {
-                    std::unique_ptr<LogicExpressionNode> opB =
-                            build(operands->Get(2));
-
-                    return std::make_unique<OrExpressionNode>(std::move(opA),
-                            std::move(opB));
-                }
-            }
-        } else if (negation->Next()) {
-            SC_LOG_INFO("not")
-
-            //not, make NotExpressionNode
-            if (operands->Next()) {
-                std::unique_ptr<LogicExpressionNode> op =
-                        build(operands->Get(2));
-
-                return std::make_unique<NotExpressionNode>(std::move(op));
-            }
-        }
-        SC_LOG_INFO("rerr")
-
-        throw runtime_error("[INFERENCE_MODULE] Unrecognized element of condition tree");
-
-        return nullptr;
-    }
+    std::unique_ptr<LogicExpressionNode> build(ScAddr node);
 
     const std::vector<ScTemplateParams>& GetParamsSet() const {
         return paramsSet;
-    }
-
-    ScAddr GetReferenceTemplate() const {
-        return referenceTemplate;
     }
 
     LogicExpressionNode& GetRoot() {
@@ -302,7 +118,6 @@ public:
 
 private:
     std::vector<ScTemplateParams> paramsSet;
-    ScAddr referenceTemplate;
     std::unique_ptr<LogicExpressionNode> root;
     ScAddr conditionRoot;
     ScMemoryContext* context;
