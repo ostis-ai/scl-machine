54ad4136eaac384bae1ead746794189deffe02f5
kilativ-dotcom <vitalik.tsishchanka@gmail.com>
2023-01-17 11:15:16 +0300

    [refactor][test]: rename tests

--- problem-solver/cxx/inferenceModule/test/tests.cmake
+++ problem-solver/cxx/inferenceModule/test/tests.cmake
@@ -3,7 +3,7 @@ set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${SC_BIN_PATH}/inference-tests)
 include_directories(${CMAKE_CURRENT_LIST_DIR} ${SC_MEMORY_SRC} ${SC_KPM_SRC} ${SC_MACHINE_ROOT}/tools)
 
 make_tests_from_folder(${CMAKE_CURRENT_LIST_DIR}/units
-		NAME inference-module-tests-starter
+		NAME inference_module_test_starter
 		DEPENDS sc-agents-common sc-builder-lib inferenceModule
 		INCLUDES ${SC_MEMORY_SRC}/tests/sc-memory/_test ${CMAKE_CURRENT_LIST_DIR}/.. ${SC_MACHINE_ROOT}/sc-tools/sc-builder/src)
 
