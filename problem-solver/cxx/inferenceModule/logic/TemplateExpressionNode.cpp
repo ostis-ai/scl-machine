f36d20d5d6bf985e126917d53e154750611df3eb
kilativ-dotcom <vitalik.tsishchanka@gmail.com>
2023-02-13 15:09:53 +0300

    [feat][expr] After search add to replacements only nodes from current template

--- problem-solver/cxx/inferenceModule/logic/TemplateExpressionNode.cpp
+++ problem-solver/cxx/inferenceModule/logic/TemplateExpressionNode.cpp
@@ -79,6 +79,7 @@ LogicFormulaResult TemplateExpressionNode::generate(Replacements & replacements)
   LogicFormulaResult result;
   result.isGenerated = false;
   std::vector<ScTemplateParams> paramsVector = ReplacementsUtils::getReplacementsToScTemplateParams(replacements);
+  std::set<std::string> const & varNames = templateSearcher->getVarNames(formulaTemplate);
   if (paramsVector.empty())
   {
     SC_LOG_DEBUG("Atomic logical formula " + context->HelperGetSystemIdtf(formulaTemplate) + " is not generated");
@@ -103,7 +104,23 @@ LogicFormulaResult TemplateExpressionNode::generate(Replacements & replacements)
       {
         result.isGenerated = true;
         result.value = true;
-        result.replacements = replacements;
+        Replacements temporalReplacements;
+        for (auto const & name : varNames)
+        {
+          ScAddrVector replacementsVector;
+          bool const generationHasVar =
+              generationResult.GetReplacements().find(name) != generationResult.GetReplacements().cend();
+          ScAddr outResult;
+          bool const paramsHaveVar = scTemplateParams.Get(name, outResult);
+          if (generationHasVar)
+            replacementsVector.push_back(generationResult[name]);
+          else if (paramsHaveVar)
+            replacementsVector.push_back(outResult);
+          else
+            SC_THROW_EXCEPTION(utils::ScException, "generation result and template params do not have replacement for " + name);
+          temporalReplacements[name] = replacementsVector;
+        }
+        result.replacements = ReplacementsUtils::uniteReplacements(result.replacements, temporalReplacements);
       }
 
       SC_LOG_DEBUG("Atomic logical formula " + context->HelperGetSystemIdtf(formulaTemplate) + " is generated");
