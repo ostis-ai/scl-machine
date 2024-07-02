b411d3f415be59fa55e723a1e9551bd744617afe
kilativ-dotcom <vitalik.tsishchanka@gmail.com>
2024-07-02 10:31:36 +0300

    [template] Remove input structures caching

--- problem-solver/cxx/inferenceModule/searcher/templateSearcher/TemplateSearcherOnlyAccessEdgesInStructures.cpp
+++ problem-solver/cxx/inferenceModule/searcher/templateSearcher/TemplateSearcherOnlyAccessEdgesInStructures.cpp
@@ -6,8 +6,6 @@
 
 #include "keynodes/InferenceKeynodes.hpp"
 
-#include "sc-agents-common/utils/IteratorUtils.hpp"
-
 #include "TemplateSearcherOnlyAccessEdgesInStructures.hpp"
 
 namespace inference
@@ -33,31 +31,12 @@ map<std::string, std::string> TemplateSearcherOnlyAccessEdgesInStructures::getTe
   return {};
 }
 
-void TemplateSearcherOnlyAccessEdgesInStructures::prepareBeforeSearch()
-{
-  this->contentOfAllInputStructures->clear();
-  if (replacementsUsingType == REPLACEMENTS_ALL)
-  {
-    SC_LOG_DEBUG("start input structures processing");
-    for (auto const & inputStructure : inputStructures)
-    {
-      ScAddrVector const & edges =
-          utils::IteratorUtils::getAllWithType(context, inputStructure, ScType::EdgeAccess);
-      contentOfAllInputStructures->insert(edges.cbegin(), edges.cend());
-    }
-    SC_LOG_DEBUG("input structures processed, found " << contentOfAllInputStructures->size() << " edges");
-  }
-}
-
 bool TemplateSearcherOnlyAccessEdgesInStructures::isValidElement(ScAddr const & element) const
 {
-  if (replacementsUsingType == REPLACEMENTS_ALL)
-    return !context->GetElementType(element).BitAnd(ScType::EdgeAccess) || contentOfAllInputStructures->count(element);
-  else
-    return !context->GetElementType(element).BitAnd(ScType::EdgeAccess) ||
-           std::any_of(
-               inputStructures.cbegin(), inputStructures.cend(), [&element, this](ScAddr const & inputStructure) {
-                 return context->HelperCheckEdge(inputStructure, element, ScType::EdgeAccessConstPosPerm);
-               });
+  return !context->GetElementType(element).BitAnd(ScType::EdgeAccess) ||
+         std::any_of(
+             inputStructures.cbegin(), inputStructures.cend(), [&element, this](ScAddr const & inputStructure) -> bool {
+               return context->HelperCheckEdge(inputStructure, element, ScType::EdgeAccessConstPosPerm);
+             });
 }
 }  // namespace inference
