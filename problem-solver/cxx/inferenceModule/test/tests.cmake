ae9c145ef31907d332ecaa27a81549b6757f80e3
kilativ-dotcom <vitalik.tsishchanka@gmail.com>
2024-09-03 18:45:17 +0300

    [cmake] Use target in include_directories

--- problem-solver/cxx/inferenceModule/test/tests.cmake
+++ problem-solver/cxx/inferenceModule/test/tests.cmake
@@ -1,9 +1,7 @@
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${SC_BIN_PATH}/inference-tests)
 
-include_directories(${CMAKE_CURRENT_LIST_DIR} ${SC_MEMORY_SRC} ${SC_KPM_SRC} ${SC_MACHINE_ROOT}/tools)
-
 make_tests_from_folder(${CMAKE_CURRENT_LIST_DIR}/units
 		NAME inference-module-tests-starter
 		DEPENDS sc-agents-common sc-builder-lib inferenceModule
-		INCLUDES ${SC_MEMORY_SRC}/tests/sc-memory/_test ${SC_MACHINE_ROOT}/sc-tools/sc-builder/src)
+		INCLUDES ${SC_MEMORY_SRC}/tests/sc-memory/_test ${SC_MACHINE_ROOT}/sc-tools/sc-builder/src ${CMAKE_CURRENT_LIST_DIR}/..)
 add_definitions(-DTEMPLATE_SEARCH_MODULE_TEST_SRC_PATH="${CMAKE_CURRENT_LIST_DIR}")
--- problem-solver/cxx/solutionModule/test/tests.cmake
+++ problem-solver/cxx/solutionModule/test/tests.cmake
@@ -1,9 +1,7 @@
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${SC_BIN_PATH}/inference-tests)
 
-include_directories(${CMAKE_CURRENT_LIST_DIR} ${SC_MEMORY_SRC} ${SC_KPM_SRC} ${SC_MACHINE_ROOT}/tools)
-
 make_tests_from_folder(${CMAKE_CURRENT_LIST_DIR}/units
 		NAME solution-module-tests-starter
 		DEPENDS sc-agents-common sc-builder-lib solutionModule
-		INCLUDES ${SC_MEMORY_SRC}/tests/sc-memory/_test ${SC_MACHINE_ROOT}/sc-tools/sc-builder/src)
+		INCLUDES ${SC_MEMORY_SRC}/tests/sc-memory/_test ${SC_MACHINE_ROOT}/sc-tools/sc-builder/src ${CMAKE_CURRENT_LIST_DIR}/..)
 add_definitions(-DSOLUTION_MODULE_TEST_SRC_PATH="${CMAKE_CURRENT_LIST_DIR}")
