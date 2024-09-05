69d0ae410235f0ddc0d84511c72e86f739444c0a
kilativ-dotcom <vitalik.tsishchanka@gmail.com>
2024-09-05 10:17:38 +0300

    [cmake] make source include public

--- problem-solver/cxx/solutionModule/test/tests.cmake
+++ problem-solver/cxx/solutionModule/test/tests.cmake
@@ -3,5 +3,5 @@ set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${SC_BIN_PATH}/inference-tests)
 make_tests_from_folder(${CMAKE_CURRENT_LIST_DIR}/units
 		NAME solution-module-tests-starter
 		DEPENDS sc-agents-common sc-builder-lib solutionModule
-		INCLUDES ${SC_MEMORY_SRC}/tests/sc-memory/_test ${SC_MACHINE_ROOT}/sc-tools/sc-builder/src ${CMAKE_CURRENT_LIST_DIR}/..)
+		INCLUDES ${SC_MEMORY_SRC}/tests/sc-memory/_test ${SC_MACHINE_ROOT}/sc-tools/sc-builder/src)
 add_definitions(-DSOLUTION_MODULE_TEST_SRC_PATH="${CMAKE_CURRENT_LIST_DIR}")
